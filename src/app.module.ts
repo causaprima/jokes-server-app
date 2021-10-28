3d1d826a03f222926b980ce8748d8a47fadecf6e
Arkady Seleznev <a.seleznev@me.com>
2021-10-29 01:34:50 +0700

    feat: init server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsersModule} from "./users/users.module";
+import {JokesModule} from "./jokes/jokes.module";
+import {AuthModule} from "./auth/auth.module";
 
 @Module({
-  imports: [],
+  imports: [
+      JokesModule,
+      UsersModule,
+      AuthModule,
+      ConfigModule.forRoot( {
+        envFilePath: '.env'
+      }),
+      GraphQLModule.forRoot( {
+          installSubscriptionHandlers: true,
+          autoSchemaFile: 'schema.gql',
+          playground: true,
+          introspection: true,
+      }),
+      TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRES_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          database: process.env.POSTGRES_DB,
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true,
+      })
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import {ValidationPipe} from "./pipes/validation.pipe";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.DEFAULT_APP_PORT || 5000
+  const hostname = process.env.DEFAULT_APP_HOSTNAME || 'localhost'
+
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(port, hostname, () => console.log(`Server running at ${hostname}:${port}`))
 }
 bootstrap();
